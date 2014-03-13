bbb9ddb28df8700f75587b7450442bd59b46c0b4
Archana S <archana.june@gmail.com>
2014-03-13 13:36:12 -0700

    Fixed the random movements. Looks fluid

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ $(document).ready(function(){
     var layer = new Kinetic.Layer();
     var allCircles = [];
     var listOfPositions = [];
-    var NUM_CIRCLES = 10;
+    var listOfMotionFunctions = [];
+    var NUM_CIRCLES = 50;
 
     for (var i=0; i < NUM_CIRCLES; i++) {
 
@@ -28,25 +29,20 @@ $(document).ready(function(){
       stage.add(layer);
       allCircles.push(circle);
       listOfPositions.push(getRandomLocation(stage.getWidth(), stage.getHeight()));
+      listOfMotionFunctions.push({xFunc: getRandomMotionFunction(), yFunc: getRandomMotionFunction()});
     }
 
     // Add animations
     var velocity = 150;
-    var period = 5000;
+    var period = 25000;
 
     var anim = new Kinetic.Animation(function(frame) {
       for (var i=0; i<allCircles.length; i++) {
-
         var pos = listOfPositions[i];
+        var motions = listOfMotionFunctions[i];
 
-        if (i % 2 === 0) {
-          xVal = (velocity * Math.sin((-1) * frame.time * 2 * (Math.PI / period))) + pos.x;
-          yVal = (velocity * Math.sin(frame.time * 2 * (Math.PI / period))) + pos.y;
-        }
-        else {
-          xVal = velocity * Math.cos(frame.time * 2 * (Math.PI / period)) + pos.x;
-          yVal = velocity * Math.cos(frame.time * 2 * (Math.PI / period)) + pos.y;
-        }
+        xVal = motions.xFunc(velocity, frame.time, period) + pos.x;
+        yVal = motions.yFunc(velocity, frame.time, period) + pos.y;
 
         allCircles[i].setX(xVal);
         allCircles[i].setY(yVal);
@@ -55,23 +51,33 @@ $(document).ready(function(){
     anim.start();
 });
 
-var getRandomMotionFunction = function(velocity, frameTime, period) {
-  var val = Math.round(Math.random() * 4);
+var getRandomMotionFunction = function() {
+  var val = Math.round(Math.random() * 3);
   switch(val) {
     case 0:
-      return velocity * Math.sin(frameTime * 2 * (Math.PI / period));
+      return function(velocity, frameTime, period) {
+                return velocity * Math.sin(frameTime * 2 * (Math.PI / period))
+              };
       break;
     case 1:
-      return velocity * Math.cos(frame.time * 2 * (Math.PI / period));
+      return function(velocity, frameTime, period) {
+                return velocity * Math.cos(frameTime * 2 * (Math.PI / period))
+              };
       break;
     case 2:
-      return velocity * Math.sin(-1 * frameTime * 2 * (Math.PI / period));
+      return function(velocity, frameTime, period) {
+                return velocity * Math.sin(-1 * frameTime * 2 * (Math.PI / period))
+              };
       break;
     case 3:
-      return velocity * Math.cos(-1 * frameTime * 2 * (Math.PI / period));
+      return function(velocity, frameTime, period) {
+                return velocity * Math.cos(-1 * frameTime * 2 * (Math.PI / period))
+              };
       break;
     default:
-      return velocity * Math.sin(frameTime * 2 * (Math.PI / period));
+      return function(velocity, frameTime, period) {
+                return velocity * Math.sin(frameTime * 2 * (Math.PI / period))
+              };
       break;
   };
 };
